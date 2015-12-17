4e89af1c207d300e0a131429bed259e98d23fad8
Pablo <Pablo@Lenovo-PC>
2015-12-18 00:46:18 +0100

    36-29EjRet

--- Ej-Variados/src/EjExtra/binDec.java
+++ Ej-Variados/src/EjExtra/binDec.java
@@ -1,5 +1,20 @@
 package EjExtra;
 
+import java.util.Scanner;
+
 public class binDec {
+
+	public static void main(String[] args) {
+		int dec = 0, aux, cont = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Introduzca un número en binario (ceros y unos): ");
+		int bin = scanner.nextInt();
+		dec = 0;
+		for (aux = bin; aux > 0; cont++) {
+			dec += (aux % 10) * Math.pow(2, cont);
+			aux /= 10;
+		}
+		System.out.println(bin + "(2 = " + dec + "(10");
+	}
 
 }
--- Ej-Variados/src/EjExtra/numPerfecto.java
+++ Ej-Variados/src/EjExtra/numPerfecto.java
@@ -1,5 +1,24 @@
 package EjExtra;
 
+import java.util.Scanner;
+
 public class numPerfecto {
 
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int sumaDivisores = 0;
+		System.out.print("Por favor, introduzca un numero natural: ");
+		int numero = scanner.nextInt();
+		scanner.close();
+		for (int a = 1; a < numero; a++) {
+			if (numero % a == 0) {
+				sumaDivisores = sumaDivisores + a;
+			}
+		}
+		if (numero == sumaDivisores) {
+			System.out.println("El numero " + numero + " es perfecto!");
+		} else {
+			System.out.println("El numero " + numero + " NO es perfecto!");
+		}
+	}
 }
